09258d669216f91164ab8193189e361ca26a6fe5
EliasValk <46247153+EliasVal@users.noreply.github.com>
2021-09-02 18:11:30 +0300

    Build Errors

--- svelte.config.js
+++ svelte.config.js
@@ -11,10 +11,11 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: adapter({ assets: 'build/D-Bugger' }),
-    // paths: {
-    //   base: '/./D-Bugger',
-    // },
+    adapter: adapter(),
+    paths: {
+      base: '/D-Bugger',
+      assets: '/D-Bugger',
+    },
     // appDir: 'internal',
     ssr: false,
     vite: {
