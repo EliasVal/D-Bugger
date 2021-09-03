4d40865cb20291170947542633d117e106b92769
EliasVal <46247153+EliasVal@users.noreply.github.com>
2021-09-03 19:43:22 +0300

    Dynamic routing fixes

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: adapter({}),
+    adapter: adapter({ fallback: '200.html' }),
     paths: {
       base: '/D-Bugger',
       assets: '/D-Bugger',
