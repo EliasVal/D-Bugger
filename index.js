c7a224a0fbdda835329b347b5d1e567db23d774b
EliasValk <46247153+EliasVal@users.noreply.github.com>
2021-09-19 10:53:26 +0300

    Imporved Notification Sender

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var admin = require("firebase-admin");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const admin = require("firebase-admin");
 
 require("dotenv").config();
 
@@ -9,43 +13,79 @@ admin.initializeApp({
 
 let failedUsers = [];
 
-admin
-  .database()
-  .ref("/users/")
-  .once("value", (usersSnapshot) => {
-    let count = 0;
-    usersSnapshot.forEach((u) => {
-      console.log(`Sending Message to ${u.key}...`);
-      admin
-        .database()
-        .ref(`/users/${u.key}/inbox`)
-        .push({
-          // DO NOT CHANGE THESE
-          type: "message",
-          read: "false",
-          // -------------------
-
-          title: "Update!",
-          description: "New update is out!",
-          // Supporst Markdown
-          content:
-            "This is a test message for an update we're more than happy to announce! come on in!",
-        })
-        .then(() => {
-          count++;
-          console.log(`Message sent successfully to ${u.key}!`);
-          if (count == usersSnapshot.numChildren()) {
-            console.log("Finished sending messages to all users!\n\n");
-            if (failedUsers.length > 0) {
-              console.log("Failed Sending messages to:");
-              console.log(failedUsers);
-            }
-            process.exit();
+readline.question(
+  "Select the type of message:\n[1] Opt-in notification | (Default)\n[2] Forced notification\n",
+  (choice) => {
+    if (!choice.match(/^[1-2]$/)) choice = "1";
+    admin
+      .database()
+      .ref("/users/")
+      .once("value", (usersSnapshot) => {
+        let count = 0;
+        usersSnapshot.forEach((u) => {
+          if (choice == "1") {
+            admin
+              .database()
+              .ref(`users/${u.key}/optInMessages`)
+              .get()
+              .then((v) => {
+                if (v.val())
+                  sendMessageToUser(u.key).then(() => {
+                    count++;
+                    compareAndClose(count, usersSnapshot.numChildren());
+                  });
+                else {
+                  count++;
+                  compareAndClose(count, usersSnapshot.numChildren());
+                }
+              });
+          } else {
+            sendMessageToUser(u.key).then(() => {
+              count++;
+              compareAndClose(count, usersSnapshot.numChildren());
+            });
           }
-        })
-        .catch(() => {
-          failedUsers.push(u.key);
-          count++;
         });
-    });
+      });
+  }
+);
+
+function compareAndClose(count, length) {
+  if (count == length) {
+    console.log("Finished sending messages to all users!\n\n");
+    if (failedUsers.length > 0) {
+      console.log("Failed Sending messages to:");
+      console.log(failedUsers);
+    }
+    process.exit();
+  }
+}
+
+function sendMessageToUser(UID) {
+  console.log(`\x1b[33mSending Message to ${UID}...\x1b[0m`);
+  return new Promise((resolve, reject) => {
+    admin
+      .database()
+      .ref(`/users/${UID}/inbox`)
+      .push({
+        // DO NOT CHANGE THESE
+        type: "message",
+        read: "false",
+        // -------------------
+
+        title: "Update!",
+        description: "New update is out!",
+        // Supporst Markdown
+        content:
+          "This is a test message for an update we're more than happy to announce! come on in!",
+      })
+      .then(() => {
+        console.log(`\x1b[32mMessage sent successfully to ${UID}!\x1b[0m`);
+      })
+      .catch(() => {
+        console.log(`\x1b[31mFailed to send message to ${UID}!\x1b[0m`);
+        failedUsers.push(UID);
+      })
+      .finally(() => resolve());
   });
+}
